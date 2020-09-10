9e0191198a01cbc3b99227e5460ae5a6390c48b0
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-09-10 23:41:53 +0200

    start rendering

--- RenderProject/RenderProject/Class1.cs
+++ RenderProject/RenderProject/Class1.cs
@@ -13,37 +13,41 @@ namespace RenderProject
     {
         public void FromVegas(Vegas myVegas)
         {
+            RenderTemplate renderTemplate;
             String inputFilePath = "F:\\videos\\vlogs\\15 - Live API 3\\projet.veg";
             if (myVegas.OpenProject(inputFilePath))
             {
-                foreach (Renderer renderer in myVegas.Renderers)
-                {
-                    String rendererName = renderer.FileTypeName;
-
-                    if (rendererName == "Windows Media Video V11")
-                    {
-                        foreach (RenderTemplate template in renderer.Templates)
-                        {
-                            if (template.Name == "Vidéo HD 1080-30p 8 Mbits/s")
-                            {
-                                String templateName = template.Name;
-                                MessageBox.Show(templateName);
-                            }
-                        }
-                    }
-                }
-                /*
-            RenderArgs renderArgs = new RenderArgs(myVegas.Project);
-            RenderTemplate renderTemplate = new RenderTemplate();
-            renderArgs.RenderTemplate = renderTemplate;
-            String outputFilePath = "F:\\videos\\vlogs\\15 - Live API 3\\projet.veg";
-            renderArgs.OutputFile = outputFilePath;
-            RenderStatus renderStatus = myVegas.Render(renderArgs); */
+                String rendererName = "Windows Media Video V11";
+                String templateName = "Vidéo HD 1080-30p 8 Mbits/s";
+                RenderArgs renderArgs = new RenderArgs(myVegas.Project);
+                renderArgs.RenderTemplate = findTemplate(rendererName, templateName, myVegas.Renderers);
+                String outputFilePath = "F:\\videos\\vlogs\\15 - Live API 3\\projet.veg";
+                renderArgs.OutputFile = outputFilePath;
+                RenderStatus renderStatus = myVegas.Render(renderArgs);
             }
             else
             {
                 myVegas.Exit();
             }
+        }
+
+        public RenderTemplate findTemplate(String rendererName, String templateName, Renderers renderers)
+        {
+            foreach (Renderer renderer in renderers)
+            {
+                if (renderer.FileTypeName == rendererName)
+                {
+                    foreach (RenderTemplate template in renderer.Templates)
+                    {
+                        if (template.Name == templateName)
+                        {
+                            return template;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("Template not found");
         }
     }
 }
