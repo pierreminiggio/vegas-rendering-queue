a725c133e4848fd2e270568f5b9e18b264caf136
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-09-10 21:37:39 +0200

    hello vegas version

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using ScriptPortal.Vegas;
+
+namespace RenderFiles
+{
+    public class EntryPoint
+    {
+        public void FromVegas(Vegas myVegas)
+        {
+            MessageBox.Show(myVegas.Version);
+        }
+    }
+}
