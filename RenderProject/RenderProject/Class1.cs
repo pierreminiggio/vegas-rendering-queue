1e8ca7334024c11c46a4712c450d65ff2bc4aa01
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-09-10 23:26:26 +0200

    find renderer

--- RenderProject/RenderProject/Class1.cs
+++ RenderProject/RenderProject/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,17 +13,29 @@ namespace RenderProject
     {
         public void FromVegas(Vegas myVegas)
         {
-            string inputFilePath = "F:\\videos\\vlogs\\15 - Live API 3\\projet.veg";
+            String inputFilePath = "F:\\videos\\vlogs\\15 - Live API 3\\projet.veg";
             if (myVegas.OpenProject(inputFilePath))
             {
-                RenderArgs renderArgs = new RenderArgs(myVegas.Project);
-                //RenderTemplate renderTemplate = new RenderTemplate();
-                //renderArgs.RenderTemplate = renderTemplate;
-                renderArgs.ShowOpenButtonsOnComplete = false;
-                renderArgs.SaveAsMono = false;
-                string outputFilePath = "F:\\videos\\vlogs\\15 - Live API 3\\projet.veg";
-                renderArgs.OutputFile = outputFilePath;
-                //RenderStatus renderStatus = myVegas.Render(renderArgs);
+                foreach (Renderer renderer in myVegas.Renderers)
+                {
+                    String rendererName = renderer.FileTypeName;
+
+                    if (rendererName == "Windows Media Video V11")
+                    {
+                        foreach (RenderTemplate template in renderer.Templates)
+                        {
+                            
+                        }
+                    }
+                    MessageBox.Show(rendererName);
+                }
+                /*
+            RenderArgs renderArgs = new RenderArgs(myVegas.Project);
+            RenderTemplate renderTemplate = new RenderTemplate();
+            renderArgs.RenderTemplate = renderTemplate;
+            String outputFilePath = "F:\\videos\\vlogs\\15 - Live API 3\\projet.veg";
+            renderArgs.OutputFile = outputFilePath;
+            RenderStatus renderStatus = myVegas.Render(renderArgs); */
             }
             else
             {
