dd996a43f8cb695982c3d6276209607031d2db18
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-09-11 06:37:04 +0200

    add fs + use config for videos

--- main.js
+++ main.js
@@ -1,8 +1,71 @@
 const {exec} = require('child_process')
+const fs = require('fs');
 
-const startRender = exec('bash start.sh')
+/**
+ * @typedef {Object} Video
+ * @property {string} projectFilePath
+ * @property {string} rendererName
+ * @property {string} templateName
+ * @property {string} outputFilePath
+ */
 
-startRender.addListener('exit', (code) => {
-    console.log(code)
-    console.log('exit')
-})
+/**
+ * @typedef {Object} Config
+ * @property {string} vegasPath
+ * @property {Video[]} videos
+ */
+
+/**
+ * @param {Array} array 
+ * @param {CallableFunction} callback 
+ */
+async function asyncForEach (array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
+
+/**
+ * @param {Video[]} videos 
+ * @param {string} vegasPath
+ */
+async function renderVideos(videos, vegasPath) {
+    await asyncForEach(videos, async (video) => {
+        return await renderVideo(video, vegasPath)
+    })
+}
+
+/**
+ * @param {Video} video 
+ * @param {string} vegasPath
+ */
+async function renderVideo(video, vegasPath) {
+    return await new Promise((resolve, reject) => {
+        fs.writeFile(
+            './tmp.csv',
+            video.projectFilePath + ';' + video.rendererName + ';' + video.templateName + ';' + video.outputFilePath,
+            (err) => {
+                if (err) {
+                    reject(err)
+                }
+
+                const startRender = exec('bash start.sh')
+                startRender.addListener('exit', (code) => {
+                    resolve(code)
+                })
+            }
+        )
+    });
+}
+
+
+fs.readFile('./config.json', 'utf8', (err, configString) => {
+    if (err) {
+        throw 'Config File Missing : ' + err
+    }
+
+    /** @var {Config} */
+    const config = JSON.parse(configString)
+    
+    renderVideos(config.videos, config.vegasPath)
+});
